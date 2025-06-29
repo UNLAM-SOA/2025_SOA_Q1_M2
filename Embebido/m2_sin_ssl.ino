b95e70fbcb972b2b51ada1de235d0149e96980b3
Alba <albanesigaston@gmail.com>
2025-06-29 20:25:31 -0300

    se agrego para publicar en /app_persiana el estado

--- Embebido/m2_sin_ssl.ino
+++ Embebido/m2_sin_ssl.ino
@@ -1,6 +1,7 @@
 #include <WiFiClientSecure.h>
 #include <WiFi.h>
 #include "PubSubClient.h"
+#include <ArduinoJson.h>
 
 #define LIGHT_SENSOR_PIN 32
 #define MOTOR_FORWARD_PIN 26
@@ -25,27 +26,31 @@ WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
 // CONFIG RED
-const char * ssid = "SO Avanzados";
-const char * password = "SOA.2019";
+const char *ssid = "SO Avanzados";
+const char *password = "SOA.2019";
 
 // CONFIG MQTT
-const char * mqtt_server = "192.168.30.116";
+const char *mqtt_server = "192.168.30.116";
 const int port = 1883;
 
-const char * user_name = "m2";
-const char * user_pass = "SOA.2019";
-const char * clientId = "m2wokwi";
+const char *user_name = "m2";
+const char *user_pass = "SOA.2019";
+const char *clientId = "m2wokwi";
 
 // TOPICOS MQTT
-const char * topic_persiana = "/persiana";
+const char *topic_persiana = "/persiana";
+const char *topicAppPersiana = "/app_persiana";
 char topic_persiana_subscribe[200];
 
-enum Mode {
+enum Mode
+{
   AUTO,
-  MANUAL
+  MANUAL,
+  MODE_QUANTITY
 };
 
-enum Cmd {
+enum Cmd
+{
   OPEN,
   CLOSE,
   STOP,
@@ -55,7 +60,8 @@ enum Cmd {
   CMD_QUANTITY
 };
 
-enum State {
+enum State
+{
   STOPPED,
   FORWARDING,
   BACKWARDING,
@@ -64,7 +70,8 @@ enum State {
   STATE_QUANTITY
 };
 
-enum Event {
+enum Event
+{
   EVT_GO_UP,
   EVT_GO_DOWN,
   EVT_PAUSE,
@@ -84,7 +91,7 @@ void dcMotorBackward();
 void dcMotorForward();
 boolean isLigthOn();
 
-// FUNCIONES PARA LOS EVENTOS 
+// FUNCIONES PARA LOS EVENTOS
 void goUp();
 void goDown();
 void mGoUp();
@@ -108,34 +115,37 @@ void cmdGoUp();
 void cmdInvalid();
 
 // TAREAS QUE LEEN SENSORES
-void lightSensorTask(void * p);
-void fcTask(void * p);
-void cmdTask(void * p);
+void lightSensorTask(void *p);
+void fcTask(void *p);
+void cmdTask(void *p);
 
 // FUNCIONES MQTT
 void wifiConnect();
-void mqttTask(void * p);
+void mqttTask(void *p);
 
 typedef void (*Function)();
 
 Function transitionMatrix[STATE_QUANTITY][EVENT_QUANTITY] = {
-  //                  EVT_GO_UP EVT_GO_DOWN   EVT_PAUSE           EVT_FC_END    EVT_FC_START EVT_MGO_UP  EVT_MGO_DOWN  EVT_CHANGE_MODE_MANUAL EVT_CHANGE_MODE_AUTO
-  /* STOPPED */     { goUp,     goDown,       noChange,           noChange,     noChange,    mGoUp,      mGoDown,      changeModeManual,      changeModeAuto },
-  /* FORWARDING */  { noChange, goDown,       changeModeManual,   pauseMotor,   noChange,    noChange,   noChange,     changeModeManual,      noChange       },
-  /* BACKWARDING */ { goUp,     noChange,     changeModeManual,   noChange,     pauseMotor,  noChange,   noChange,     changeModeManual,      noChange       },
-  /* MFORWARDING */ { noChange, noChange,     pauseMotor,         pauseMotor,   noChange,    noChange,   mGoDown,      noChange,              changeModeAuto },
-  /* MBACKWARDING */{ noChange, noChange,     pauseMotor,         noChange,     pauseMotor,  mGoUp,      noChange,     noChange,              changeModeAuto }
- };
+    //                  EVT_GO_UP EVT_GO_DOWN   EVT_PAUSE           EVT_FC_END    EVT_FC_START EVT_MGO_UP  EVT_MGO_DOWN  EVT_CHANGE_MODE_MANUAL EVT_CHANGE_MODE_AUTO
+    /* STOPPED */ {goUp, goDown, noChange, noChange, noChange, mGoUp, mGoDown, changeModeManual, changeModeAuto},
+    /* FORWARDING */ {noChange, goDown, changeModeManual, pauseMotor, noChange, noChange, noChange, changeModeManual, noChange},
+    /* BACKWARDING */ {goUp, noChange, changeModeManual, noChange, pauseMotor, noChange, noChange, changeModeManual, noChange},
+    /* MFORWARDING */ {noChange, noChange, pauseMotor, pauseMotor, noChange, noChange, mGoDown, noChange, changeModeAuto},
+    /* MBACKWARDING */ {noChange, noChange, pauseMotor, noChange, pauseMotor, mGoUp, noChange, noChange, changeModeAuto}};
 
-//                                   OPEN     CLOSE      STOP      MODE_MANUAL MODE_AUTO INVALID_CMD      
-Function cmdActions[CMD_QUANTITY] = {cmdGoUp, cmdGoDown, cmdPause, cmdManual,  cmdAuto,  cmdInvalid};
+//                                   OPEN     CLOSE      STOP      MODE_MANUAL MODE_AUTO INVALID_CMD
+Function cmdActions[CMD_QUANTITY] = {cmdGoUp, cmdGoDown, cmdPause, cmdManual, cmdAuto, cmdInvalid};
+
+String stateStrings[STATE_QUANTITY] = {"DETENIDA", "ABIERTA", "CERRADA", "ABIERTA", "CERRADA"};
+String modeStrings[MODE_QUANTITY] = {"AUTO", "MANUAL"};
 
 State currentState = STOPPED;
 Event currentEvent = EVT_PAUSE;
 Mode currentConfig = MANUAL;
 QueueHandle_t eventQueue;
 
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_MONITOR);
   Serial.println("SISOP AV UNLAM M2 Q1 2025 - Cortina Roller 1");
 
@@ -146,8 +156,8 @@ void setup() {
   pinMode(FC_END_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  dcMotorStop(); // iniciliza motor apagado
-  dcMotorSpeed(); // setea speed 
+  dcMotorStop();  // iniciliza motor apagado
+  dcMotorSpeed(); // setea speed
 
   eventQueue = xQueueCreate(MAX_EVENTS_QUEUE, sizeof(Event));
   xTaskCreate(lightSensorTask, "Sensor de Luz", STACK_SIZE, NULL, PRIORITY_LIGHT_SENSOR, NULL);
@@ -161,196 +171,243 @@ void setup() {
 
   client.setServer(mqtt_server, port);
   client.setCallback(callback);
-
 }
 
-void loop() {
+void loop()
+{
   stateMachine();
 }
 
 // MAQUINA DE ESTADOS
-void stateMachine() {
+void stateMachine()
+{
   getEvent();
   transitionMatrix[currentState][currentEvent]();
 }
 
-void getEvent() {
+void getEvent()
+{
   Event newEvent;
-  if((xQueueReceive(eventQueue, &newEvent, portMAX_DELAY)) == pdPASS){
-    if(currentEvent == EVT_FC_END && newEvent == EVT_GO_UP) return;
-    if(currentEvent == EVT_FC_START && newEvent == EVT_GO_DOWN) return;
+  if ((xQueueReceive(eventQueue, &newEvent, portMAX_DELAY)) == pdPASS)
+  {
+    if (currentEvent == EVT_FC_END && newEvent == EVT_GO_UP)
+      return;
+    if (currentEvent == EVT_FC_START && newEvent == EVT_GO_DOWN)
+      return;
     currentEvent = newEvent;
   }
 }
 
 // FUNCIONES PARA ACTUALIZAR Y LEER LOS PINES
-boolean isLigthOn() {
+boolean isLigthOn()
+{
   int lightValue = analogRead(LIGHT_SENSOR_PIN);
   return lightValue >= LIGHT_THRESHOLD;
 }
 
-void dcMotorForward() {
+void dcMotorForward()
+{
   digitalWrite(MOTOR_FORWARD_PIN, HIGH);
   digitalWrite(MOTOR_BACKWARD_PIN, LOW);
   digitalWrite(LED_PIN, HIGH);
 }
 
-void dcMotorBackward() {
+void dcMotorBackward()
+{
   digitalWrite(MOTOR_FORWARD_PIN, LOW);
   digitalWrite(MOTOR_BACKWARD_PIN, HIGH);
   digitalWrite(LED_PIN, HIGH);
 }
 
-void dcMotorSpeed() {
+void dcMotorSpeed()
+{
   analogWrite(MOTOR_SPEED_PIN, SPEED);
 }
 
-void dcMotorStop() {
+void dcMotorStop()
+{
   digitalWrite(MOTOR_FORWARD_PIN, LOW);
   digitalWrite(MOTOR_BACKWARD_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
 }
 
-// FUNCIONES PARA LOS EVENTOS 
-void goUp() {
+// FUNCIONES PARA LOS EVENTOS
+void goUp()
+{
   currentState = FORWARDING;
   dcMotorForward();
 }
 
-void goDown() {
+void goDown()
+{
   currentState = BACKWARDING;
   dcMotorBackward();
 }
 
-void mGoUp() {
+void mGoUp()
+{
   currentState = MFORWARDING;
   dcMotorForward();
 }
 
-void mGoDown() {
+void mGoDown()
+{
   currentState = MBACKWARDING;
   dcMotorBackward();
 }
 
-void changeModeManual() {
+void changeModeManual()
+{
   currentConfig = MANUAL;
   pauseMotor();
 }
 
-void changeModeAuto() {
+void changeModeAuto()
+{
   currentConfig = AUTO;
   pauseMotor();
 }
 
-void pauseMotor() {
+void pauseMotor()
+{
   currentState = STOPPED;
   dcMotorStop();
 }
 
-void noChange() {
+void noChange()
+{
   // Do nothing
 }
 
 // FUNCIONES PARA MANEJAR LOS COMANDOS
-Cmd cmdMapper(String cmd) {
+Cmd cmdMapper(String cmd)
+{
   cmd.toLowerCase();
-  if (cmd == "abrir") return OPEN;
-  if (cmd == "cerrar") return CLOSE;
-  if (cmd == "pausar") return STOP;
-  if (cmd == "cm manual") return MODE_MANUAL;
-  if (cmd == "cm auto") return MODE_AUTO;
+  if (cmd == "abrir")
+    return OPEN;
+  if (cmd == "cerrar")
+    return CLOSE;
+  if (cmd == "pausar")
+    return STOP;
+  if (cmd == "cm manual")
+    return MODE_MANUAL;
+  if (cmd == "cm auto")
+    return MODE_AUTO;
   return INVALID_CMD;
 }
 
-void cmdGoUp() {
-  if(currentConfig == MANUAL) {
+void cmdGoUp()
+{
+  if (currentConfig == MANUAL)
+  {
     Event evt;
-    if(digitalRead(FC_END_PIN) == LOW){
+    if (digitalRead(FC_END_PIN) == LOW)
+    {
       evt = EVT_MGO_UP;
       xQueueSend(eventQueue, &evt, TIME_OUT);
       Serial.println("Abriendo cortina");
     }
-    else {
+    else
+    {
       Serial.println("La cortina ya esta completamente abierta");
     }
   }
 }
 
-void cmdGoDown() {
-  if(currentConfig == MANUAL){
+void cmdGoDown()
+{
+  if (currentConfig == MANUAL)
+  {
     Event evt;
-    if(digitalRead(FC_START_PIN) == LOW){
+    if (digitalRead(FC_START_PIN) == LOW)
+    {
       evt = EVT_MGO_DOWN;
       xQueueSend(eventQueue, &evt, TIME_OUT);
       Serial.println("Cerrando cortina");
     }
-    else {
+    else
+    {
       Serial.println("La cortina ya esta completamente cerrada");
-    }  
+    }
   }
 }
 
-void cmdPause() {
+void cmdPause()
+{
   Event evt = EVT_PAUSE;
   xQueueSend(eventQueue, &evt, TIME_OUT);
   Serial.println("La cortina se a detenido");
 }
 
-void cmdManual() {
-  if(currentConfig == AUTO) {
+void cmdManual()
+{
+  if (currentConfig == AUTO)
+  {
     Event evt = EVT_CHANGE_MODE_MANUAL;
     xQueueSend(eventQueue, &evt, TIME_OUT);
     Serial.println("Se cambio a modo MANUAL");
   }
-  else {
+  else
+  {
     Serial.println("Ya se encuentra en modo manual");
   }
 }
 
-void cmdAuto() {
-  if(currentConfig == MANUAL) {
+void cmdAuto()
+{
+  if (currentConfig == MANUAL)
+  {
     Event evt = EVT_CHANGE_MODE_AUTO;
     xQueueSend(eventQueue, &evt, TIME_OUT);
     Serial.println("Se cambio a modo AUTO");
   }
-  else {
+  else
+  {
     Serial.println("Ya se encuentra en modo AUTO");
   }
 }
 
-void cmdInvalid() {
+void cmdInvalid()
+{
   Serial.println("Comando no reconocido");
 }
 
-
 // TAREAS QUE LEEN SENSORES
-void lightSensorTask(void *p) {
+void lightSensorTask(void *p)
+{
   const TickType_t delayTimeOut = 5000;
   Event evt;
 
-  while (true) {
+  while (true)
+  {
     vTaskDelay(delayTimeOut);
-    if (currentConfig != AUTO) continue;
+    if (currentConfig != AUTO)
+      continue;
 
     bool lightOn = isLigthOn();
     int pinState = digitalRead(lightOn ? FC_END_PIN : FC_START_PIN);
 
-    if (pinState == HIGH) continue;
+    if (pinState == HIGH)
+      continue;
 
     evt = lightOn ? EVT_GO_UP : EVT_GO_DOWN;
     xQueueSend(eventQueue, &evt, TIME_OUT);
   }
 }
 
-void fcTask(void *p) {
+void fcTask(void *p)
+{
   Event evt;
   TickType_t delayTimeOut = 200;
-  while (true) {
-    if(digitalRead(FC_END_PIN) == HIGH) {
+  while (true)
+  {
+    if (digitalRead(FC_END_PIN) == HIGH)
+    {
       evt = EVT_FC_END;
       xQueueSend(eventQueue, &evt, TIME_OUT);
     }
-    if(digitalRead(FC_START_PIN) == HIGH) {
+    if (digitalRead(FC_START_PIN) == HIGH)
+    {
       evt = EVT_FC_START;
       xQueueSend(eventQueue, &evt, TIME_OUT);
     }
@@ -358,10 +415,13 @@ void fcTask(void *p) {
   }
 }
 
-void cmdTask(void *p) {
+void cmdTask(void *p)
+{
   TickType_t delayTimeOut = 500;
-  while(true) {
-    if (Serial.available()) {
+  while (true)
+  {
+    if (Serial.available())
+    {
       String console_str = Serial.readStringUntil('\n');
       console_str.trim();
       Cmd cmd = cmdMapper(console_str);
@@ -371,9 +431,12 @@ void cmdTask(void *p) {
   }
 }
 
-void mqttTask(void *p) {
-  while (true) {
-    if (!client.connected()) {
+void mqttTask(void *p)
+{
+  while (true)
+  {
+    if (!client.connected())
+    {
       mqttReconnect();
     }
     client.loop();
@@ -381,29 +444,31 @@ void mqttTask(void *p) {
   }
 }
 
-
 // FUNCIONES MQTT
-void wifiConnect() {
+void wifiConnect()
+{
   Serial.println("+ Conectando a WiFi");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     vTaskDelay(500 / portTICK_PERIOD_MS);
     Serial.print(".");
   }
   Serial.println(" ==> Conectado.");
 }
 
-void mqttReconnect() {
+void mqttReconnect()
+{
   while (!client.connected())
   {
     Serial.println("+ Intentando conexión MQTT...");
 
-    if (client.connect(clientId, user_name, user_pass)) 
+    if (client.connect(clientId, user_name, user_pass))
     {
       Serial.printf("==> '%s' conectado\n", clientId);
       client.subscribe(topic_persiana);
     }
-    else 
+    else
     {
       Serial.printf("Error, rc= %d. Intentando en 5 segundos\n", client.state());
       vTaskDelay(5000 / portTICK_PERIOD_MS);
@@ -411,14 +476,27 @@ void mqttReconnect() {
   }
 }
 
-void callback(char * topic, byte * message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   String msg;
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     msg += (char)message[i];
   }
 
   Serial.printf("+ Mensaje recibido: %s\n", msg);
-
+  // Ejecuto comando
   Cmd cmd = cmdMapper(String(msg));
   cmdActions[cmd]();
+  // Publico el estado actual de la persiana
+  client.publish(topicAppPersiana, generatePayload());
+}
+
+char *generatePayload()
+{
+  String payload = "{";
+  payload += "\"estado\": \"" + stateStrings[currentState] + "\", ";
+  payload += "\"modo\": \"" + modeStrings[currentConfig] + "\", ";
+  payload += "}";
+  return payload.c_str();
 }
